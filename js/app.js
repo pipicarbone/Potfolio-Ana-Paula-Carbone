0aba8c3c460ca6165187fd49c17e8b825a2c92ee
piponashe <pipicarbone@gmail.com>
2025-08-14 18:21:15 -0300

    terminando detalles

--- js/app.js
+++ js/app.js
@@ -6,3 +6,23 @@ function moverPlantilla(lado) {
   inicio = (inicio + lado + totalP) % totalP;
   proyectos.style.transform = `translateX(-${inicio * 700}px)`;
 }
+
+
+const navLinks = document.querySelectorAll('header nav a');
+
+navLinks.forEach(link => {
+  link.addEventListener('click', (evento) => {
+
+    evento.preventDefault();
+    
+    const Id = link.getAttribute('href');
+    
+    const tarjeta = document.querySelector(Id);
+    
+    if (tarjeta) {
+      tarjeta.scrollIntoView({
+        behavior: 'smooth' 
+      });
+    }
+  });
+});
